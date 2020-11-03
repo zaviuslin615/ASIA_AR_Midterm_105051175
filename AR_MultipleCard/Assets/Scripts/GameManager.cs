6b94f1e563462ec5004c1ad2c5a569b8d8fe20d7
zaviuslin615 <71679933+zaviuslin615@users.noreply.github.com>
2020-11-03 12:03:50 +0800

    控制物件旋轉與縮放

--- AR_MultipleCard/Assets/Scripts/GameManager.cs
+++ AR_MultipleCard/Assets/Scripts/GameManager.cs
@@ -2,7 +2,30 @@
 
 public class GameManager : MonoBehaviour
 {
-   public Transform Rabbit;
+    [Header ("史萊姆")]
+   public Transform Slime;
+    [Header("蝙蝠")]
    public Transform Bat;
-   
+    [Header("虛擬搖桿")]
+   public FixedJoystick Joystick;
+    [Header("旋轉速度") , Range(0.1f, 20f)]
+    public float turn = 1.5f;
+    [Header("縮放"), Range(0f, 5f)]
+    public float size = 0.3f;
+
+    private void Start()
+    {
+        print("開始事件執行中");
+    }
+
+    private void Update()
+    {
+        print("更新事件");
+        print(Joystick.Vertical);
+
+        Slime.Rotate(0, Joystick.Horizontal * turn, 0);
+        Bat.Rotate(0, Joystick.Horizontal * turn, 0);
+
+        Slime.localScale += new Vector3(1, 1, 1) * Joystick.Vertical * size;
+    }
 }
