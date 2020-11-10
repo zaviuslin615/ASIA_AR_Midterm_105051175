6e773f6e689f81f18b650be25ca59240d774afb5
zaviuslin615 <71679933+zaviuslin615@users.noreply.github.com>
2020-11-10 12:01:41 +0800

    完成與發佈

--- AR_MultipleCard/Assets/Scripts/GameManager.cs
+++ AR_MultipleCard/Assets/Scripts/GameManager.cs
@@ -12,10 +12,14 @@ public class GameManager : MonoBehaviour
     public float turn = 1.5f;
     [Header("縮放"), Range(0f, 5f)]
     public float size = 0.3f;
+    [Header("史萊姆動畫元件")]
+    public Animator aniSlime;
+    [Header("蝙蝠動畫元件")]
+    public Animator aniBat;
 
     private void Start()
     {
-        print("開始事件執行中");
+        print("開始事件");
     }
 
     private void Update()
@@ -27,5 +31,23 @@ public class GameManager : MonoBehaviour
         Bat.Rotate(0, Joystick.Horizontal * turn, 0);
 
         Slime.localScale += new Vector3(1, 1, 1) * Joystick.Vertical * size;
+        Slime.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(Slime.localScale.x, 5f, 35f);
+        Bat.localScale += new Vector3(1, 1, 1) * Joystick.Vertical * size;
+        Bat.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(Slime.localScale.x, 5f, 35f);
     }
+
+    public void Attack()
+    {
+        print("攻擊");
+        aniSlime.SetTrigger("攻擊觸發");
+        aniBat.SetTrigger("攻擊觸發");
+    }
+
+    public void PlayAnimation(string aniName)
+    {
+        print(aniName);
+        aniSlime.SetTrigger(aniName);
+        aniBat.SetTrigger(aniName);
+    }
+
 }
